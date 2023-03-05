0e4980fa9208b3087536809f7b10d62f801fa59d
evelyn-2022 <evelyn20220101@gmail.com>
2023-03-05 18:11:08 +0800

    finished lab6

--- lab6/capers/Dog.java
+++ lab6/capers/Dog.java
@@ -1,17 +1,17 @@
 package capers;
 
 import java.io.File;
+import java.io.IOException;
 import java.io.Serializable;
 import static capers.Utils.*;
 
 /** Represents a dog that can be serialized.
- * @author TODO
+ * @author Evelyn
 */
-public class Dog { // TODO
+public class Dog implements Serializable {
 
     /** Folder that dogs live in. */
-    static final File DOG_FOLDER = null; // TODO (hint: look at the `join`
-                                         //      function in Utils)
+    static final File DOG_FOLDER = Utils.join(new File(System.getProperty("user.dir")), ".capers/dogs");
 
     /** Age of dog. */
     private int age;
@@ -39,8 +39,8 @@ public class Dog { // TODO
      * @return Dog read from file
      */
     public static Dog fromFile(String name) {
-        // TODO (hint: look at the Utils file)
-        return null;
+        File f = new File(DOG_FOLDER, name);
+        return readObject(f, Dog.class);
     }
 
     /**
@@ -56,7 +56,15 @@ public class Dog { // TODO
      * Saves a dog to a file for future use.
      */
     public void saveDog() {
-        // TODO (hint: don't forget dog names are unique)
+        File d = new File(DOG_FOLDER.toURI());
+        d.mkdirs();
+        File f = new File(DOG_FOLDER, name);
+        try {
+            f.createNewFile();
+            writeObject(f, this);
+        } catch(IOException ie) {
+            ie.printStackTrace();
+        }
     }
 
     @Override
